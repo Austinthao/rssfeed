c14c46a2f623e4e8c7dd9535fd7c8ae242260acf
Austin Thao <athao@csumb.edu>
2018-01-18 23:46:53 +0000

    wireframe-ish?

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ var options = {
     port: 443, 
     path: '',
     method: 'GET'
-}
+};
 
 var path120 = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2F25livepub.collegenet.com%2Fcalendars%2F012-120-digital-sign.rss&api_key=' + process.env.API_KEY;
 var path148 = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2F25livepub.collegenet.com%2Fcalendars%2F012-148-digital-sign.rss&api_key=' + process.env.API_KEY;
@@ -20,60 +20,6 @@ var path108 = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2F25livep
 //RSS for room 148 http://25livepub.collegenet.com/calendars/012-148-digital-sign.rss
 //RSS for room 113 http://25livepub.collegenet.com/calendars/012-113-digital-sign.rss
 //RSS for room 108 http://25livepub.collegenet.com/calendars/012-108-digital-sign.rss
-
-function rssStu120(sendResponseToBrowser){
-    
-    var apiResponse = '';
-    var rssreq = https.get(options, function(response){
-        
-        rssreq.on('data', function(chunk){
-            console.log("received data: "); 
-            apiResponse += chunk; 
-            
-            
-        });
-        
-        rssreq.on('end', function() {
-            var feed = JSON.parse(apiResponse);
-            
-            sendResponseToBrowser(feed);
-            
-        });
-        
-        
-    });
-    
-    rssreq.end();
-}
-
-
-function rssStu148(sendResponseToBrowser){
-    
-    options.path = 'https://api.rss2json.com/v1/api.json?rss_url=http%3A%2F%2F25livepub.collegenet.com%2Fcalendars%2F012-148-digital-sign.rss&api_key=' + process.env.API_KEY;
-    var apiResponse = '';
-    var rssreq = https.get(options, function(response){
-        
-        rssreq.on('data', function(chunk){
-            console.log("received data: "); 
-            apiResponse += chunk; 
-            
-            
-        });
-        
-        rssreq.on('end', function() {
-            var feed = JSON.parse(apiResponse);
-            
-            sendResponseToBrowser(feed);
-            
-        });
-        
-        
-    });
-    
-    rssreq.end();
-}
-
-
 
 function getrss(sendResponseToBrowser) {
     
